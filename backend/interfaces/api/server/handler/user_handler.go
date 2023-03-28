0c1dacc424df5eba36966024278ad197939fa77b
Hikaru_Kaneko <hikarukanekoeg@gmail.com>
2023-03-28 18:37:07 +0900

    errorhandler

--- backend/interfaces/api/server/handler/user_handler.go
+++ backend/interfaces/api/server/handler/user_handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -32,38 +34,63 @@ func (h *userHandler) Router() chi.Router {
 	return r
 }
 
-// func errorHandler(err error) {
-// 	logger.Log.Sugar().Errorf("Internal Error: %w", err)
-// }
+func errorHandler(err error, w http.ResponseWriter, r *http.Request) {
+	logger.Log.Sugar().Debugf("Application error: %s", err.Error())
+
+	if rec := recover(); rec != nil {
+		logger.Log.Sugar().Errorf("panic: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	switch  {
+	case errors.Is(err, ERR_BAD_REQUEST):
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	case errors.Is(err, ERR_NOT_FOUND):
+		w.WriteHeader(http.StatusNotFound)
+		return
+	default:
+		logger.Log.Sugar().Errorf("Internal Server Error: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+}
 
 func (h *userHandler) Get() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.Background()
 
+		var err error
+		defer func() {
+			if err != nil {
+				errorHandler(err, w, r)
+				return
+			}
+		}()
+
 		id, err := strconv.Atoi(chi.URLParam(r, "id"))
 		if err != nil {
-			logger.Log.Sugar().Errorf("failed to atoi id err=%s\n", err.Error())
-			w.WriteHeader(http.StatusBadRequest)
+			err = fmt.Errorf("failed to atoi id err=%w", err)
 			return
 		}
 
 		user, err := h.useCase.Get(ctx, id)
 		if err != nil {
-			logger.Log.Sugar().Errorf("failed to Get err=%s\n", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
+			err = fmt.Errorf("failed to Get err=%s", err.Error())
 			return
 		}
 
 		if user == nil {
-			w.WriteHeader(http.StatusNotFound)
+			err = ERR_NOT_FOUND
 			return
 		}
 
 		res := &GetUserResponse{user}
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		if err := json.NewEncoder(w).Encode(res); err != nil {
-			logger.Log.Sugar().Errorf("failed to Encode response err=%s\n", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
+			err = fmt.Errorf("failed to Encode response err=%w", err)
 			return
 		}
 	}
